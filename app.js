ce1efa2cca2fe96f10507be36ee539f544f1d426
mpadil18 <mpadil18@ucsc.edu>
2021-06-11 00:58:22 -0700

    Poster Title and Rating

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 console.log("page loaded");
 
 //Global Constants
-const apiKey = "15c5939e9b145d8147a78e01270d0614";
+const apiKey = "OOGABOOGA";
 const limit = 9;
 const rating = "g";
 
@@ -14,6 +14,8 @@ const search = document.querySelector("#search-bar");
 const submit = document.querySelector("#submit");
 const results = document.querySelector(".results");
 const movieArea = document.querySelector("#movie-area");
+
+const playingURL = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&page=1`;
 
 // 1. control form submit behavior with JS
 movieForm.addEventListener("submit", getResults);
@@ -34,23 +36,19 @@ async function getResults(evt) {
     const response = await fetch(apiUrl);
     const responseData = await response.json();
     console.log("response stuff", response, responseData.results);
-    responseData.results.forEach(element => displayMovies(element.poster_path));
+    responseData.results.forEach(element => displayMovies(element.poster_path, element.original_title, element.vote_average));
     //responseData.results.forEach(console.log("Poop"));
     /*console.log(responseData.results[0]);
     console.log(responseData.results[1]);
     console.log(responseData.results[2]);*/
 }
 
-async function displayMovies(movieData) {
-    let myUrl = movieData;
-    /*movieArea.innerHTML += `https://api.themoviedb.org/3/movie/${movieData.id}/images?api_key=${apiKey}&language=en-US
-      <img src="${myUrl}" alt="movie"/>
-    `;*/
-    movieArea.innerHTML += `
-      <img src="https://image.tmdb.org/t/p/w500${movieData}" alt="Gif"/>
-    `;
+function displayMovies(poster, title, rating) {
+    movieArea.innerHTML = movieArea.innerHTML + `<div class="myMovie">
+      <img src="https://image.tmdb.org/t/p/w500${poster}" class="pic" alt="${title}"/>` + `<br><div class="title">${title}</div>` + `<div class="rating">${rating}</div></myMovie>`
+    ;
   } 
-
+  
 function handleFormSubmit(evt) {
     movieArea.innerHTML = "";
     const term = apiUrl + movie.value;
