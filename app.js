a3c413e377cb5c7e483bcf24483beb4a5aefeddc
mpadil18 <mpadil18@ucsc.edu>
2021-06-11 17:00:52 -0700

    Edit styling

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 console.log("page loaded");
 
 //Global Constants
-const apiKey = "OOGABOOGA";
+const apiKey = "15c5939e9b145d8147a78e01270d0614";
 const limit = 9;
 const rating = "g";
 
@@ -12,10 +12,14 @@ const attribution = document.querySelector("#attribution");
 const topper = document.querySelector(".top");
 const search = document.querySelector("#search-bar");
 const submit = document.querySelector("#submit");
+const reset = document.querySelector("#reset");
 const results = document.querySelector(".results");
-const movieArea = document.querySelector("#movie-area");
+var movieArea = document.querySelector("#movie-area");
 
-const playingURL = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&page=1`;
+const loadMore = document.querySelector(".loadMore");
+var page = 1;
+var movieName = "";
+const playingURL = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&page=${page}`;
 
 //Display default without form submit
 window.onload = function () {
@@ -23,14 +27,37 @@ window.onload = function () {
 }
 // 1. control form submit behavior with JS
 movieForm.addEventListener("submit", getResults);
+loadMore.addEventListener("click", showMore);
+
+reset.addEventListener("click", function() {
+  movieArea.innerHTML = "";
+  getPlayingResults();
+});
+
+function showMore() {
+  page++;
+  getMoreResults();
+}
+
+async function getMoreResults() {
+  const playingURL = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&page=${page}`;
+  console.log("api URL is", playingURL, page);
+  // 2. On form submit, go to the Giphy API
+  const response = await fetch(playingURL);
+  const responseData = await response.json();
+  console.log("response stuff", response, responseData.results);
+  responseData.results.forEach(element => displayMovies(element.poster_path, element.original_title, element.vote_average));
+  loadMore.classList.remove("hidden");
+}
 
 //Create a new function, getResults, that get results from the API. 
 async function getResults(evt) {
     evt.preventDefault();
     const movieInput = evt.target.searchBar;
     const movie = movieInput.value;
+    movieName = movie;
     console.log("Movie search", movie)
-    const apiSearchUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&page=1&include_adult=false&query=${movie}
+    const apiSearchUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&page=${page}&include_adult=false&query=${movie}
     //`;
     console.log("api URL is", apiSearchUrl);
         
@@ -38,11 +65,13 @@ async function getResults(evt) {
     const response = await fetch(apiSearchUrl);
     const responseData = await response.json();
     console.log("response stuff", response, responseData.results);
+    movieArea.innerHTML = "";
     responseData.results.forEach(element => displayMovies(element.poster_path, element.original_title, element.vote_average));
+    loadMore.classList.add("hidden");  
 }
 
 async function getPlayingResults() {
-  const apiPlayingUrl = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&page=1
+  const apiPlayingUrl = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&page=${page}
   `;
   console.log("api URL is", apiPlayingUrl);
       
@@ -50,12 +79,14 @@ async function getPlayingResults() {
   const response = await fetch(apiPlayingUrl);
   const responseData = await response.json();
   console.log("response stuff", response, responseData.results);
+  movieArea.innerHTML = "";
   responseData.results.forEach(element => displayMovies(element.poster_path, element.original_title, element.vote_average));
+  loadMore.classList.remove("hidden");
 }
 
 function displayMovies(poster, title, rating) {
     movieArea.innerHTML = movieArea.innerHTML + `<div class="myMovie">
-      <img src="https://image.tmdb.org/t/p/w500${poster}" class="pic" alt="${title}"/>` + `<br><div class="title">${title}</div>` + `<div class="rating">${rating}</div></myMovie>`
+      <img src="https://image.tmdb.org/t/p/w500${poster}" class="pic" alt="${title}"/>` + `<br><div class="title">${title}</div>` + `<div class="rating"><img src="https://www.pinclipart.com/picdir/big/31-311255_star-clipart-star-with-black-background-png-download.png" width="10px" height="10px"/>${rating}</div></myMovie>`
     ;
   } 
   
